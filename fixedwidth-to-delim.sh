c575fc40eaef98b50d514cafe5b17540ada55d29
tiaano <40819441+tiaano@users.noreply.github.com>
2018-09-13 10:32:01 +0200

    add gzip part

--- fixedwidth-to-delim.sh
+++ fixedwidth-to-delim.sh
@@ -8,3 +8,11 @@ echo "Col1|Col2|Col3" > NEW_FILE.txt
 
 # Extract and add data (-b is to extract specific bytes from file, starting with 1)
 cat FILE1.txt | cut --output-delimiter="|" -b 88-95,98,99-107 >> NEW_FILE.txt
+
+#-------------------------------------------------------------------------
+# Do the above from a gzip file strait to another gzip file
+
+# Set headers
+echo "Col1|Col2|Col3" | gxip > NEW_FILE.txt.gz
+gunzip -c FILE1.txt.gz | cut --output-delimiter="|" -b 88-95,98,99-107 | gzip >> NEW_FILE.txt.gz
+
